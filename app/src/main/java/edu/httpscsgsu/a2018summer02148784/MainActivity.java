bc73938e41621b326d324e90d8c9c39ce8e22a7d
Jimmyvo121 <jimmyvo121@gmail.com>
2018-06-04 12:53:45 -0400

    test

---
+++ app/src/main/java/edu/httpscsgsu/a2018summer02148784/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.httpscsgsu.a2018summer02148784;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+        setContentView(R.layout.activity_main);
+    }
+}
