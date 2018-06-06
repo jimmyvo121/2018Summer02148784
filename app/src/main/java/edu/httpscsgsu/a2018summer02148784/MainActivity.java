8ccb7e5bf752675158902fc3d1237d9ce03bc7ee
Jimmyvo121 <jimmyvo121@gmail.com>
2018-06-06 11:24:18 -0400

    test

--- app/src/main/java/edu/httpscsgsu/a2018summer02148784/MainActivity.java
+++ app/src/main/java/edu/httpscsgsu/a2018summer02148784/MainActivity.java
@@ -2,13 +2,17 @@ package edu.httpscsgsu.a2018summer02148784;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView();
+
         setContentView(R.layout.activity_main);
+        RelativeLayout b = findViewById(R.id.main_title_layout);
+        Toast.makeText(this,"hello, android!", Toast.LENGTH_LONG);
     }
 }
